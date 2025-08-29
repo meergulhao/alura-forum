71fd2bd6f8b5e44d4a14cc6ad78c9eeb33d432ec
Guilherme Mergulhão <golimpiof@gmail.com>
2025-08-29 17:15:35 -0300

    Adiciona filtro por nome, e paginação

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -6,6 +6,12 @@ import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.service.TopicoService
 import jakarta.transaction.Transactional
 import jakarta.validation.Valid
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
+import org.springframework.data.web.PagedModel
+import org.springframework.data.web.config.EnableSpringDataWebSupport
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.util.UriComponentsBuilder
@@ -24,8 +31,11 @@ import org.springframework.web.util.UriComponentsBuilder
 class TopicoController(private val service: TopicoService) {
 
     @GetMapping
-    fun listar(): List<TopicoView> {
-        return service.listar()
+    fun listar(@RequestParam(required = false)
+               nomeCurso: String?,
+               @PageableDefault(size = 10, sort = ["dataCriacao"], direction = Sort.Direction.DESC)
+               pagiacao: Pageable): Page<TopicoView> {
+        return service.listar(nomeCurso, pagiacao)
     }
 
     @GetMapping("/{id}")
